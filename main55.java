1e91496eb12a20ab3d7bcf4dd03d51b91de8699e
linsir <linsir678@gmail.com>
2017-09-21 16:19:19 +0800

    添加刷题代码55

---
+++ main55.java
@@ -0,0 +1,10 @@
+class Main55 {
+    
+    public boolean canJump(int[] nums) {
+        int reach = nums[0];
+        for(int i = 1; i < nums.length && reach >= i; i++)
+            if(i + nums[i] > reach) reach = i + nums[i];  //贪心策略
+        return reach >= (nums.length-1) ? true : false;
+    }
+}
+
