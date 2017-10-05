9b6812918c82ebadf1f5a3db346e989a08742715
linsir <linsir678@gmail.com>
2017-10-05 23:04:34 +0800

    添加刷题代码100

---
+++ main100.java
@@ -0,0 +1,17 @@
+
+public class Main100 {
+
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null){
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }else {
+            return false;
+        }
+    }
+}
