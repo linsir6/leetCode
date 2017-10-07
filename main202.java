512244911212d6a0f845b9d756f328f041cd10d6
linsir <linsir678@gmail.com>
2017-10-07 23:02:42 +0800

    添加刷题代码202

---
+++ main202.java
@@ -0,0 +1,28 @@
+
+public class Main202 {
+
+	public boolean isHappy(int n) {
+        if (n == 0) return false;
+        if (n == 1) return true;
+        return number(n);
+    }
+
+    public boolean number(int n) {
+        int count = n;
+        HashSet<Integer> set = new HashSet<Integer>();
+        while (set.add(count)) {
+            int a = 0;
+            while (count > 0) {
+                int num = count % 10;
+                a += num * num;
+                count = count / 10;
+            }
+            if (a == 1) {
+                return true;
+            } else {
+                count = a;
+            }
+        }
+        return false;
+    }
+}
