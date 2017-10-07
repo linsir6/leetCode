2acbd1d886066a20887dcf7650fba466b97b45e0
linsir <linsir678@gmail.com>
2017-10-07 23:02:30 +0800

    添加刷题代码69

---
+++ main69.java
@@ -0,0 +1,29 @@
+
+public class Main69 {
+
+	public int mySqrt(int x) {
+
+        if (x <= 1) {
+            return x;
+        }
+
+        int begin = 1;
+        int end = x;
+        int middle = 0;
+
+        while (begin <= end) {
+            middle = begin + (end - begin) / 2;
+            if (middle == x / middle) {
+                return middle;
+            } else {
+                if (middle < x / middle) {
+                    begin = middle + 1;
+                } else {
+                    end = middle - 1;
+                }
+            }
+
+        }
+        return end;
+    }
+}
